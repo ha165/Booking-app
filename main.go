8c611170e21d0e6324ca21da3779795971b9059d
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-10-30 09:50:41 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to  my quiz  application!")
+ 	var confrenceName string  = "Go Confrence"
+ 	const confrenceTickets int  = 50
+ 	var remainingTickets uint = 50
 
-	var name string = "Harmony"
+	fmt.Printf("Welcome to %v booking Application!\n", confrenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confrenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend,")
+
+	var userName string
+	var userTickets uint
+
+	fmt.Println("Enter your name:")
+	fmt.Scan(&userName)
+
+	userTickets = 2
+
+	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email shortly.\n", userName, userTickets)
 }
