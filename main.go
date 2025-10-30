d384d29d60cecde11ecac45a648fb85ac96d0fe6
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-10-30 10:50:45 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,17 +7,31 @@ func main() {
  	const confrenceTickets int  = 50
  	var remainingTickets uint = 50
 
+	var bookings [50]string
+	bookings[0] = "Sajid"
+	bookings[1] = "Ahmed"
+	
+
 	fmt.Printf("Welcome to %v booking Application!\n", confrenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confrenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend,")
 
 	var userName string
 	var userTickets uint
+	var userEmail string
+
 
 	fmt.Println("Enter your name:")
 	fmt.Scan(&userName)
 
-	userTickets = 2
+	fmt.Println("Enter your email:")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
 
 	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email shortly.\n", userName, userTickets)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, confrenceName)
 }
